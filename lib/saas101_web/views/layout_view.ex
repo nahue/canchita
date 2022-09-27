e6c89ca989ea8c612124fa38059faa81f8a69e74
Nahuel Chaves <nahuel.chaves@gmail.com>
2022-09-27 19:39:10 -0300

    update

--- lib/saas101_web/views/layout_view.ex
+++ lib/saas101_web/views/layout_view.ex
@@ -1,6 +1,10 @@
 defmodule Saas101Web.LayoutView do
   use Saas101Web, :view
 
+  def is_active(conn, path) do
+    List.last(conn.path_info) == path
+  end
+
   # Phoenix LiveDashboard is available only in development by default,
   # so we instruct Elixir to not warn if the dashboard route is missing.
   @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
