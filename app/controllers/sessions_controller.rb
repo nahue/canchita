780b84d564f9f0246507d55c68da8d42ade1677b
Nahuel Chaves <nahuel.chaves@gmail.com>
2023-10-21 01:33:43 -0300

    update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate, only: %i[ new create ]
 
   before_action :set_session, only: :destroy
+  layout "auth"
 
   def index
     @sessions = Current.user.sessions.order(created_at: :desc)
