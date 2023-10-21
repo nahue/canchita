608f9c67fd9b1d68b603e71b190a8f1025c275d1
Nahuel Chaves <nahuel.chaves@gmail.com>
2023-10-21 00:57:46 -0300

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+#
+v = Venue.find_or_create_by!(name: "The Venue") do |v|
+  v.cover.attach(
+    io:  File.open(File.join(Rails.root,'app/assets/images/multi-sport-4.jpg')),
+    filename: 'photo.jpg'
+  )
+end
+
+v = Venue.find_or_create_by!(name: "Cochocho Vargas") do |v|
+  v.cover.attach(
+    io:  File.open(File.join(Rails.root,'app/assets/images/cochocho_vargas.jpg')),
+    filename: 'photo.jpg'
+  )
+end
